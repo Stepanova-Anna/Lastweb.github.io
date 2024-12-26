f9497ee470059ff46d27cfc6b1f4f6407516e8d9
Stepanova-Anna <st.ann0@yandex.ru>
2024-12-26 14:08:50 +0300

    Initial commit

---
+++ precache-manifest.f57cb9cb2ce8a75851917b66efff919f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b7084d6b29904144a0b2183fcda223",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed5a7fd491ef2dc3830",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "1ed5a7fd491ef2dc3830",
+    "url": "/static/js/main.63182305.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
